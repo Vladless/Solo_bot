3f5c449817c86a9c891a3a1177760fe1054e08f6
Vlad <curov2014@yandex.ru>
2024-10-03 07:08:17 +0300

    чистота кода

--- auth.py
+++ auth.py
@@ -1,14 +1,15 @@
 import json
+
 import requests
 
-from config import SERVERS  # Импортируем SERVERS из config.py
+from config import SERVERS
 
 session = None
 
 def login_with_credentials(server_id: str, username: str, password: str):
     global session
     session = requests.Session()
-    api_url = SERVERS[server_id]['API_URL']  # Получаем API_URL для выбранного сервера
+    api_url = SERVERS[server_id]['API_URL']
     auth_url = f"{api_url}/login/"
     data = {
         "username": username,
@@ -22,10 +23,10 @@ def login_with_credentials(server_id: str, username: str, password: str):
         raise Exception(f"Ошибка авторизации: {response.status_code}, {response.text}")
 
 def get_clients(session, server_id):
-    api_url = SERVERS[server_id]['API_URL']   # Получаем GET_INBOUNDS_URL для выбранного сервера
+    api_url = SERVERS[server_id]['API_URL']
     response = session.get(f'{api_url}/panel/api/inbounds/list/')
     if response.status_code == 200:
-        return response.json()  # Возвращает данные по инбаундам и клиентам
+        return response.json()
     else:
         raise Exception(f"Ошибка при получении клиентов: {response.status_code}, {response.text}")
 
@@ -45,12 +46,10 @@ def link(session, server_id: str, client_id: str, email: str):
     inbounds = response['obj'][0]
     settings = json.loads(inbounds['settings'])
     
-    # Найти клиентский ID в настройках
     stream_settings = json.loads(inbounds['streamSettings'])
     tcp = stream_settings.get('network', 'tcp')
     reality = stream_settings.get('security', 'reality')
     flow = stream_settings.get('flow', 'xtls-rprx-vision')
     
-    # Создание ссылки для подключения VLESS
     val = f"vless://{client_id}@{SERVERS[server_id]['DOMEN']}?type={tcp}&security={reality}&pbk={SERVERS[server_id]['PBK']}&fp=chrome&sni={SERVERS[server_id]['SNI']}&sid={SERVERS[server_id]['SID']}=%2F&flow={flow}#{SERVERS[server_id]['PREFIX']}-{email}"
     return val
--- handlers/start.py
+++ handlers/start.py
@@ -7,8 +7,7 @@ from aiogram.types import (BufferedInputFile, CallbackQuery,
                            InlineKeyboardButton, InlineKeyboardMarkup, Message)
 
 from bot import bot
-from config import (CHANNEL_URL, 
-                    SUPPORT_CHAT_URL)
+from config import CHANNEL_URL, SUPPORT_CHAT_URL
 
 router = Router()
 
@@ -16,22 +15,18 @@ class FeedbackState(StatesGroup):
     waiting_for_feedback = State()
 
 async def send_welcome_message(chat_id: int):
-    # Новый текст приветствия
     welcome_text = (
         "*SoloNet — ваш доступ в свободный интернет! 🌐✨*\n\n"
         "Используйте надежный и быстрый VPN, который гарантирует вашу безопасность даже в самых строгих условиях. 🔒🚀\n\n"
         "*ver. 1.0*"
     )
 
-    # Путь к изображению
     image_path = os.path.join(os.path.dirname(__file__), 'pic.jpg')
 
-    # Проверка существования файла
     if not os.path.isfile(image_path):
         await bot.send_message(chat_id, "Файл изображения не найден.")
         return
 
-    # Создаем inline-клавиатуру
     inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='👤 Мой профиль', callback_data='view_profile')],
         [InlineKeyboardButton(text='🔒 О VPN', callback_data='about_vpn')],
@@ -39,14 +34,13 @@ async def send_welcome_message(chat_id: int):
         [InlineKeyboardButton(text='📢 Наш канал', url=CHANNEL_URL)]
     ])
 
-    # Отправляем изображение с инлайн-клавиатурой
     with open(image_path, 'rb') as image_from_buffer:
         await bot.send_photo(
             chat_id,
             BufferedInputFile(image_from_buffer.read(), filename="pic.jpg"),
             caption=welcome_text,
             parse_mode='Markdown',
-            reply_markup=inline_keyboard  # Inline-клавиатура
+            reply_markup=inline_keyboard 
         )
 
 @router.message(Command('start'))
@@ -55,9 +49,8 @@ async def start_command(message: Message):
 
 @router.callback_query(lambda c: c.data == 'about_vpn')
 async def handle_about_vpn(callback_query: CallbackQuery):
-    # Удаляем сообщение главного меню
-    await callback_query.message.delete()
 
+    await callback_query.message.delete()
     info_message = (
         "*О VPN*\n\n"
         "Мы используем высокоскоростные серверы в разных локациях и выдаём ключ каждому индивидуально. "
@@ -78,9 +71,7 @@ async def handle_about_vpn(callback_query: CallbackQuery):
 
 @router.callback_query(lambda c: c.data == 'back_to_menu')
 async def handle_back_to_menu(callback_query: CallbackQuery):
-    # Удаляем текущее сообщение
+
     await callback_query.message.delete()
-    
-    # Отправляем приветственное сообщение
     await send_welcome_message(callback_query.from_user.id)
     await callback_query.answer()
