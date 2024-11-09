7d51ff3a85197d1b64232b7d4bd6231cc68ec791
Zakhar Izmaylov <ptdev@kedruss.ru>
2024-11-09 12:01:24 +0300

    Delete wraps

--- handlers/admin/admin_panel.py
+++ handlers/admin/admin_panel.py
@@ -17,7 +17,6 @@ from backup import backup_database
 from bot import bot
 from config import ADMIN_ID, DATABASE_URL
 from handlers.commands import send_message_to_all_clients
-from middlewares.admin import admin_only
 
 router = Router()
 
@@ -28,96 +27,100 @@ class UserEditorState(StatesGroup):
 
 
 @router.message(Command("admin"))
-@admin_only()
-async def handle_admin_command(message: types.Message):
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="Статистика пользователей", callback_data="user_stats"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="Редактор пользователей", callback_data="user_editor"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="Отправить сообщение всем клиентам",
-                    callback_data="send_to_alls",
-                )
-            ],
-            [InlineKeyboardButton(text="Создать бэкап", callback_data="backups")],
-            [
-                InlineKeyboardButton(
-                    text="Перезапустить бота", callback_data="restart_bot"
-                )
-            ],
-        ]
-    )
-    await bot.send_message(
-        message.chat.id, "Панель администратора", reply_markup=keyboard
-    )
+async def handle_admin_command(message: types.Message, is_admin: bool):
+    if is_admin:
+        keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text="Статистика пользователей", callback_data="user_stats"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Редактор пользователей", callback_data="user_editor"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Отправить сообщение всем клиентам",
+                        callback_data="send_to_alls",
+                    )
+                ],
+                [InlineKeyboardButton(text="Создать бэкап", callback_data="backups")],
+                [
+                    InlineKeyboardButton(
+                        text="Перезапустить бота", callback_data="restart_bot"
+                    )
+                ],
+            ]
+        )
+        await bot.send_message(
+            message.chat.id, "Панель администратора.", reply_markup=keyboard
+        )
+    else:
+        await bot.send_message(message.chat.id, "У вас нет доступа к этой команде.")
 
 
 @router.callback_query(F.data == "user_stats")
-@admin_only()
-async def user_stats_menu(callback_query: CallbackQuery):
-    conn = await asyncpg.connect(DATABASE_URL)
-    try:
-        total_users = await conn.fetchval("SELECT COUNT(*) FROM connections")
-        total_keys = await conn.fetchval("SELECT COUNT(*) FROM keys")
-        total_referrals = await conn.fetchval("SELECT COUNT(*) FROM referrals")
+async def user_stats_menu(callback_query: CallbackQuery, is_admin: bool):
+    if is_admin:
+        conn = await asyncpg.connect(DATABASE_URL)
+        try:
+            total_users = await conn.fetchval("SELECT COUNT(*) FROM connections")
+            total_keys = await conn.fetchval("SELECT COUNT(*) FROM keys")
+            total_referrals = await conn.fetchval("SELECT COUNT(*) FROM referrals")
 
-        active_keys = await conn.fetchval(
-            "SELECT COUNT(*) FROM keys WHERE expiry_time > $1",
-            int(datetime.utcnow().timestamp() * 1000),
-        )
-        expired_keys = total_keys - active_keys
+            active_keys = await conn.fetchval(
+                "SELECT COUNT(*) FROM keys WHERE expiry_time > $1",
+                int(datetime.utcnow().timestamp() * 1000),
+            )
+            expired_keys = total_keys - active_keys
 
-        stats_message = (
-            f"🔹 <b>Общая статистика пользователей:</b>\n"
-            f"• Всего пользователей: <b>{total_users}</b>\n"
-            f"• Всего ключей: <b>{total_keys}</b>\n"
-            f"• Всего рефералов: <b>{total_referrals}</b>\n"
-            f"• Активные ключи: <b>{active_keys}</b>\n"
-            f"• Истекшие ключи: <b>{expired_keys}</b>"
-        )
+            stats_message = (
+                f"🔹 <b>Общая статистика пользователей:</b>\n"
+                f"• Всего пользователей: <b>{total_users}</b>\n"
+                f"• Всего ключей: <b>{total_keys}</b>\n"
+                f"• Всего рефералов: <b>{total_referrals}</b>\n"
+                f"• Активные ключи: <b>{active_keys}</b>\n"
+                f"• Истекшие ключи: <b>{expired_keys}</b>"
+            )
 
-        back_button = InlineKeyboardButton(
-            text="Назад", callback_data="back_to_admin_menu"
-        )
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[[back_button]])
+            back_button = InlineKeyboardButton(
+                text="Назад", callback_data="back_to_admin_menu"
+            )
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[back_button]])
 
-        await callback_query.message.edit_text(
-            stats_message, reply_markup=keyboard, parse_mode="HTML"
-        )
-    finally:
-        await conn.close()
-
-    await callback_query.answer()
+            await callback_query.message.edit_text(
+                stats_message, reply_markup=keyboard, parse_mode="HTML"
+            )
+        finally:
+            await conn.close()
+        await callback_query.answer()
 
 
 @router.callback_query(F.data == "send_to_alls")
-@admin_only()
-async def handle_send_to_all(callback_query: CallbackQuery, state: FSMContext):
-    await send_message_to_all_clients(callback_query.message, state, from_panel=True)
-    await callback_query.answer()
+async def handle_send_to_all(
+    callback_query: CallbackQuery, state: FSMContext, is_admin: bool
+):
+    if is_admin:
+        await send_message_to_all_clients(
+            callback_query.message, state, from_panel=True
+        )
+        await callback_query.answer()
 
 
 @router.callback_query(F.data == "backups")
-@admin_only()
-async def handle_backup(message: Message):
-    await message.answer("Запускаю бэкап базы данных...")
-    await backup_database()
-    await message.answer("Бэкап завершен и отправлен админу.")
+async def handle_backup(message: Message, is_admin: bool):
+    if is_admin:
+        await message.answer("Запускаю бэкап базы данных...")
+        await backup_database()
+        await message.answer("Бэкап завершен и отправлен админу.")
 
 
 @router.callback_query(F.data == "restart_bot")
-@admin_only()
-async def handle_restart(callback_query: CallbackQuery):
-    if callback_query.from_user.id == ADMIN_ID:
+async def handle_restart(callback_query: CallbackQuery, is_admin: bool):
+    if is_admin:
         try:
             result = subprocess.run(
                 ["sudo", "systemctl", "restart", "bot.service"],
@@ -130,47 +133,44 @@ async def handle_restart(callback_query: CallbackQuery):
             await callback_query.message.answer(
                 f"Бот будет перезапущен через 30 секунд {e.stderr}"
             )
-    else:
-        await callback_query.answer(
-            "У вас нет доступа к этой команде.", show_alert=True
-        )
 
 
 @router.callback_query(F.data == "user_editor")
-@admin_only()
-async def user_editor_menu(callback_query: CallbackQuery):
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="Поиск по имени ключа", callback_data="search_by_key_name"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="Поиск по tg_id", callback_data="search_by_tg_id"
-                )
-            ],
-            [
-                InlineKeyboardButton(text="Назад", callback_data="back_to_admin_menu")
-            ],  # Back button
-        ]
-    )
-    await callback_query.message.edit_text(
-        "Выберите метод поиска:", reply_markup=keyboard
-    )
+async def user_editor_menu(callback_query: CallbackQuery, is_admin: bool):
+    if is_admin:
+        keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text="Поиск по имени ключа", callback_data="search_by_key_name"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Поиск по tg_id", callback_data="search_by_tg_id"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Назад", callback_data="back_to_admin_menu"
+                    )
+                ],  # Back button
+            ]
+        )
+        await callback_query.message.edit_text(
+            "Выберите метод поиска:", reply_markup=keyboard
+        )
 
 
 @router.callback_query(F.data == "back_to_admin_menu")
-@admin_only()
-async def back_to_admin_menu(callback_query: CallbackQuery):
+async def back_to_admin_menu(callback_query: CallbackQuery, is_admin: bool):
     try:
         await callback_query.message.delete()
     except Exception:
         pass
 
     tg_id = callback_query.from_user.id
-    if tg_id == ADMIN_ID:
+    if is_admin:
         keyboard = InlineKeyboardMarkup(
             inline_keyboard=[
                 [
@@ -198,8 +198,6 @@ async def back_to_admin_menu(callback_query: CallbackQuery):
             ]
         )
         await bot.send_message(tg_id, "Панель администратора", reply_markup=keyboard)
-    else:
-        await bot.send_message(tg_id, "У вас нет доступа к этой команде.")
 
 
 async def handle_error(tg_id, callback_query, message):
