0a375964712ea9939a478811629eacbfd143cb57
Zakhar Izmaylov <ptdev@kedruss.ru>
2024-11-11 22:26:10 +0300

    Add stars pay(beta)

--- handlers/payment/stars_pay.py
+++ handlers/payment/stars_pay.py
@@ -1,22 +1,18 @@
-from aiocryptopay import AioCryptoPay, Networks
 from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiohttp import web
 from loguru import logger
+from telegram import LabeledPrice
 
 from bot import bot
-from config import CRYPTO_BOT_ENABLE, CRYPTO_BOT_TOKEN, RUB_TO_USDT
+from config import RUB_TO_XTR
 from database import add_connection, check_connection_exists, get_key_count, update_balance
 from handlers.profile import process_callback_view_profile
 from handlers.texts import PAYMENT_OPTIONS
 
 router = Router()
-
-if CRYPTO_BOT_ENABLE:
-    crypto = AioCryptoPay(token=CRYPTO_BOT_TOKEN, network=Networks.MAIN_NET)
 
 
 class ReplenishBalanceState(StatesGroup):
@@ -140,27 +136,20 @@ async def process_amount_selection(
     await state.set_state(ReplenishBalanceState.waiting_for_payment_confirmation)
 
     try:
-        invoice = await crypto.create_invoice(
-            asset="USDT",
-            amount=str(amount // RUB_TO_USDT),
-            description=f"Пополнения баланса на {amount//RUB_TO_USDT} руб",
-            payload=f"{callback_query.from_user.id}:{amount//RUB_TO_USDT}",
+        builder = InlineKeyboardBuilder()
+        builder.row(
+            InlineKeyboardButton(text="Пополнить", pay=True),
+            InlineKeyboardButton(text="⬅️ Назад", callback_data="back_to_profile"),
         )
-
-        if hasattr(invoice, "bot_invoice_url"):
-            builder = InlineKeyboardBuilder()
-            builder.row(
-                InlineKeyboardButton(text="Пополнить", url=invoice.bot_invoice_url),
-                InlineKeyboardButton(text="⬅️ Назад", callback_data="back_to_profile"),
-            )
-            await callback_query.message.edit_text(
-                text=f"Вы выбрали пополнение на {amount//RUB_TO_USDT} рублей.",
-                reply_markup=builder.as_markup(),
-            )
-        else:
-            await send_message_with_deletion(
-                callback_query.from_user.id, "Ошибка при создании платежа.", state=state
-            )
+        await callback_query.message.answer_invoice(
+            title=f"Вы выбрали пополнение на {amount//RUB_TO_XTR} рублей.",
+            description=f"Вы выбрали пополнение на {amount//RUB_TO_XTR} рублей.",
+            prices=[LabeledPrice(label="XTR", amount=amount)],
+            provider_token="",
+            payload=f"{amount}_stars",
+            currency="XTR",
+            reply_markup=builder.as_markup(),
+        )
     except Exception as e:
         logger.error(f"Ошибка при создании платежа: {e}")
         await callback_query.message.answer("Произошла ошибка при создании платежа.")
@@ -182,39 +171,6 @@ async def send_payment_success_notification(user_id: int, amount: float):
         logger.error(f"Ошибка при отправке уведомления пользователю {user_id}: {e}")
 
 
-async def cryptobot_webhook(request):
-    try:
-        data = await request.json()
-        logger.info(f"Получены данные вебхука: {data}")
-        if data.get("update_type") == "invoice_paid":
-            await process_crypto_payment(data["payload"])
-            return web.Response(status=200)
-        else:
-            logger.warning(
-                f"Неподдерживаемый тип обновления: {data.get('update_type')}"
-            )
-            return web.Response(status=400)
-    except Exception as e:
-        logger.error(f"Ошибка обработки вебхука: {e}")
-        return web.Response(status=500)
-
-
-async def process_crypto_payment(payload):
-    if payload["status"] == "paid":
-        custom_payload = payload["payload"]
-        user_id_str, amount_str = custom_payload.split(":")
-        try:
-            user_id = int(user_id_str)
-            amount = float(amount_str)
-            logger.debug(f"Payment succeeded for user_id: {user_id}, amount: {amount}")
-            await update_balance(user_id, amount)
-            await send_payment_success_notification(user_id, amount)
-        except ValueError as e:
-            logger.error(f"Ошибка конвертации user_id или amount: {e}")
-    else:
-        logger.warning(f"Получен неоплаченный инвойс: {payload}")
-
-
 @router.callback_query(F.data == "enter_custom_amount")
 async def process_enter_custom_amount(
     callback_query: types.CallbackQuery, state: FSMContext
@@ -228,40 +184,45 @@ async def process_enter_custom_amount(
 async def process_custom_amount_input(message: types.Message, state: FSMContext):
     if message.text.isdigit():
         amount = int(message.text)
-        if amount // 100 <= 0:
+        if amount // RUB_TO_XTR <= 0:
             await message.answer(
-                f"Сумма должна быть больше {RUB_TO_USDT}. Пожалуйста, введите сумму еще раз:"
+                f"Сумма должна быть больше {RUB_TO_XTR}. Пожалуйста, введите сумму еще раз:"
             )
             return
 
         await state.update_data(amount=amount)
         await state.set_state(ReplenishBalanceState.waiting_for_payment_confirmation)
         try:
-            invoice = await crypto.create_invoice(
-                asset="USDT",
-                amount=str(amount // RUB_TO_USDT),
-                description=f"Пополнения баланса на {amount//RUB_TO_USDT} руб",
-                payload=f"{message.from_user.id}:{amount//RUB_TO_USDT}",
+            builder = InlineKeyboardBuilder()
+            builder.row(
+                InlineKeyboardButton(text="Пополнить", pay=True),
+                InlineKeyboardButton(text="⬅️ Назад", callback_data="back_to_profile"),
             )
-
-            if hasattr(invoice, "bot_invoice_url"):
-                builder = InlineKeyboardBuilder()
-                builder.row(
-                    InlineKeyboardButton(text="Пополнить", url=invoice.bot_invoice_url),
-                    InlineKeyboardButton(
-                        text="⬅️ Назад", callback_data="back_to_profile"
-                    ),
-                )
-                await message.message.edit_text(
-                    text=f"Вы выбрали пополнение на {amount//RUB_TO_USDT} рублей.",
-                    reply_markup=builder.as_markup(),
-                )
-            else:
-                await send_message_with_deletion(
-                    message.from_user.id, "Ошибка при создании платежа.", state=state
-                )
+            await message.answer_invoice(
+                title=f"Вы выбрали пополнение на {amount//RUB_TO_XTR} рублей.",
+                description=f"Вы выбрали пополнение на {amount//RUB_TO_XTR} рублей.",
+                prices=[LabeledPrice(label="XTR", amount=amount)],
+                provider_token="",
+                payload=f"{amount}_stars",
+                currency="XTR",
+                reply_markup=builder.as_markup(),
+            )
         except Exception as e:
             logger.error(f"Ошибка при создании платежа: {e}")
             await message.answer("Произошла ошибка при создании платежа.")
     else:
         await message.answer("Некорректная сумма. Пожалуйста, введите сумму еще раз:")
+
+
+@router.message(F.successful_payment)
+async def on_successful_payment(
+    message: types.Message,
+):
+    try:
+        user_id = int(message.from_user.id)
+        amount = float(message.successful_payment.total_amount)
+        logger.debug(f"Payment succeeded for user_id: {user_id}, amount: {amount}")
+        await update_balance(user_id, amount)
+        await send_payment_success_notification(user_id, amount)
+    except ValueError as e:
+        logger.error(f"Ошибка конвертации user_id или amount: {e}")
