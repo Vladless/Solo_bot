b40bf3f83377fe7598b37bee195f6b171c8a546d
Vladless <curov2014@yandex.ru>
2024-11-24 20:19:23 +0300

    fix back cryptobot

--- handlers/payments/cryprobot_pay.py
+++ handlers/payments/cryprobot_pay.py
@@ -74,7 +74,7 @@ async def process_callback_pay_cryptobot(callback_query: types.CallbackQuery, st
             callback_data="enter_custom_amount_crypto",
         )
     )
-    builder.row(InlineKeyboardButton(text="⬅️ Назад", callback_data="back_to_profile"))
+    builder.row(InlineKeyboardButton(text="⬅️ Назад", callback_data="view_profile"))
 
     key_count = await get_key_count(tg_id)
 
