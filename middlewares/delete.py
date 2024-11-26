f18af7010f9625ad743e133cfaa1a5c3f73d1e22
Vladless <curov2014@yandex.ru>
2024-11-26 20:03:21 +0300

    fix admin

--- middlewares/delete.py
+++ middlewares/delete.py
@@ -13,7 +13,7 @@ class DeleteMessageMiddleware(BaseMiddleware):
     ) -> Any:
         if isinstance(event, (Message, CallbackQuery)):
             if isinstance(event, Message):
-                if not event.entities[0].type == "bot_command" and event.text == "/start":
+                if event.entities and event.entities[0].type == "bot_command" and event.text == "/start":
                     try:
                         await event.bot.delete_message(event.chat.id, event.message_id - 1)
                     except Exception:
