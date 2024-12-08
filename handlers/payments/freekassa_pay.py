94d335d950cb7c272dae985bfcc9de897c0cac5a
hteppl <hteppl.dev@gmail.com>
2024-12-08 05:13:38 +0300

    Move payments utils into utils folder

--- handlers/payments/freekassa_pay.py
+++ handlers/payments/freekassa_pay.py
@@ -14,7 +14,7 @@ import requests
 
 from config import FREEKASSA_API_KEY, FREEKASSA_SHOP_ID
 from utils.database import add_payment, update_balance
-from handlers.payments.utils import send_payment_success_notification
+from utils.payments.utils import send_payment_success_notification
 from handlers.texts import PAYMENT_OPTIONS
 
 router = Router()
