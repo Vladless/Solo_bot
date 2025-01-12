f11b014acfc5ff655d0abafedb334d905cd2bc74
hteppl <hteppl.dev@gmail.com>
2025-01-12 17:29:48 +0300

    Implement admin sender handler

---
+++ keyboards/admin/sender_kb.py
@@ -0,0 +1,35 @@
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from keyboards.admin.panel_kb import build_admin_back_btn
+
+
+class AdminSenderCallback(CallbackData, prefix='admin_sender'):
+    type: str
+
+
+def build_sender_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="📢 Отправить всем",
+        callback_data=AdminSenderCallback(
+            type="all"
+        ).pack()
+    )
+    builder.button(
+        text="📢 Отправить с подпиской",
+        callback_data=AdminSenderCallback(
+            type="subscribed"
+        ).pack()
+    )
+    builder.button(
+        text="📢 Отправить без подписки",
+        callback_data=AdminSenderCallback(
+            type="unsubscribed"
+        ).pack()
+    )
+    builder.row(
+        build_admin_back_btn("admin")
+    )
+    return builder.as_markup()
