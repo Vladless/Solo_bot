054d710d6c8c572a6f9c6f573fb6d41b3a8a5208
hteppl <hteppl.dev@gmail.com>
2025-01-12 17:32:11 +0300

    Register all the missing routers

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -2,9 +2,13 @@ __all__ = ("router",)
 
 from aiogram import Router
 
+from .admin_backups import router as backups_router
 from .admin_coupons import router as coupons_router
 from .admin_panel import router as panel_router
+from .admin_restart import router as restart_router
+from .admin_sender import router as sender_router
 from .admin_servers import router as servers_router
+from .admin_stats import router as stats_router
 from .admin_users import router as users_router
 
 router = Router(name="admins_main_router")
@@ -12,6 +16,10 @@ router = Router(name="admins_main_router")
 router.include_routers(
     panel_router,
     servers_router,
-    coupons_router,
     users_router,
+    stats_router,
+    backups_router,
+    sender_router,
+    coupons_router,
+    restart_router,
 )
