fa17faa424e54778dd0034f1e32da32b3cdba5f5
Vladless <curov2014@yandex.ru>
2025-04-21 22:04:07 +0300

    update version

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTM
 storage = MemoryStorage()
 dp = Dispatcher(bot=bot, storage=storage)
 
-version = "4.2-b180408"
+version = "4.2-b220422"
 
 register_middleware(dp)
 
