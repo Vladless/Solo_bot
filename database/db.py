0e63a21627b39cfdd804d0f7de3bd2f0496221c2
hteppl <hteppl.dev@gmail.com>
2025-07-30 01:07:28 +0300

    Remove unused Base variable

--- database/db.py
+++ database/db.py
@@ -1,10 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import declarative_base
 
 from config import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=False, future=True, pool_size=20, max_overflow=30, pool_timeout=15)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
-
-Base = declarative_base()
